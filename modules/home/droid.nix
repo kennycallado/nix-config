5f96cd741612d5ad160c2fef084ab46dd0bebd1f
kennycallado <kennycallado@hotmail.com>
2024-05-11 17:49:21 +0200

    UPDATE droid: font (pdt. test)

--- modules/home/droid.nix
+++ modules/home/droid.nix
@@ -1,4 +1,4 @@
-{ agenix, pkgs, lib, host, ... }:
+{ agenix, pkgs, lib, config, host, ... }:
 
 {
   imports = [
@@ -26,4 +26,13 @@
   home.file.".ssh/authorized_keys" = {
     text = host.config.user.sshPublicKey;
   };
+
+home.activation = {
+  copyFont = let
+      font_src = "${pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }}/share/fonts/truetype/NerdFonts/Fira Code Regular Nerd Font Complete Mono.ttf";
+      font_dst = "${config.home.homeDirectory}/.termux/font.ttf";
+    in lib.hm.dag.entryAfter ["writeBoundary"] ''
+      ( test ! -e "${font_dst}" || test $(sha1sum "${font_src}"|cut -d' ' -f1 ) != $(sha1sum "${font_dst}" |cut -d' ' -f1)) && $DRY_RUN_CMD install $VERBOSE_ARG -D "${font_src}" "${font_dst}"
+  '';
+};
 }
