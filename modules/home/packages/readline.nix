82af693dfa4303693033ac317b8748b36b9cac7c
kennycallado <kennycallado@hotmail.com>
2024-06-15 19:03:27 +0200

    UPDATE home: better readline config

--- modules/home/packages/readline.nix
+++ modules/home/packages/readline.nix
@@ -4,18 +4,18 @@
   programs.readline = {
     enable = true;
     extraConfig = ''
-set echo-control-characters off
-set enable-bracketed-paste on
+      set echo-control-characters off
+      set enable-bracketed-paste on
 
-set editing-mode vi
-$if mode=vi
-  set vi-ins-mode-string \1\e[6 q\2 I \1\e[6 q\2
-  set vi-cmd-mode-string \1\e[2 q\2 N \1\e[2 q\2
-  set show-mode-in-prompt on
+      set editing-mode vi
+      $if mode=vi
+        set vi-ins-mode-string \1\e[6 q\2 I \1\e[6 q\2
+        set vi-cmd-mode-string \1\e[2 q\2 N \1\e[2 q\2
+        set show-mode-in-prompt on
 
-  set keymap vi-insert
-  Control-l: clear-screen
-$endif
+        set keymap vi-insert
+        Control-l: clear-screen
+      $endif
     '';
   };
 }
